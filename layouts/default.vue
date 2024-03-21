175c3877e4989d3edef195bbebc9a34b417212ad
encarbassot <elgrefa@gmail.com>
2024-03-21 15:19:19 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Navbar from "~/components/layout/navbar.vue"
+</script>
+
+
+
+
+<template>
+  <div class="h-screen">
+    <Navbar />
+
+    <slot />
+
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  }
+}
+
