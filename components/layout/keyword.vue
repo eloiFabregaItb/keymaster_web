35ee539e685267ea5c4f41c842d30d2434a7dbf7
sergiogoju <sergio.gonzalez.7e6@itb.cat>
2024-04-17 15:14:07 +0200

    km

--- components/layout/keyword.vue
+++ components/layout/keyword.vue
@@ -1,31 +1,18 @@
 <template>
-  <div class="kw-container">
-    <div class="kw">
-      <div class="container flex flex-col justify-center items-center">
-        <div class="">
-          <div class="content-wrapper text-style mt-5 mb-5 text-white">
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
-          </div>
-          <div class="keyboard text-keyword flex-col">
-            <div class="row" v-for="(row, rowIndex) in keyboardLayout" :key="rowIndex">
-              <div v-for="(key, keyIndex) in row" :key="keyIndex"
-                :class="['key', { 'active': key === activeKey }, { 'space-key': key === 'Space' }, { 'del-key': key === 'DEL' }]"
-                :style="{ flex: key === 'Space' ? '3' : '1' }"
-                @click="handleKeyClick(key)">
-                {{ key === 'Space' ? 'Space' : key }}
-              </div>
-            </div>
+  <div class=" flex justify-center items-center">
+    <div class="container flex flex-col justify-center mb-5 items-center" style="border-top: 1.5px solid lightgrey;">
+      <div class="text-style mt-5 mb-5 text-white">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+        </p>
+      </div>
+      <div class="keyboard text-keyword flex-col">
+        <div class="row" v-for="(row, rowIndex) in keyboardLayout" :key="rowIndex">
+          <div v-for="(key, keyIndex) in row" :key="keyIndex"
+            :class="['key', { 'active': key === activeKey }, { 'space-key': key === 'Space' }]"
+            @click="handleKeyClick(key)">
+            {{ key }}
           </div>
         </div>
-      </div>
-    </div>
-    <!-- Div lateral vertical -->
-    <div class="sidebar text-white">
-      <h2 class="text-lg font-semibold mb-2">Usuarios</h2>
-      <div class="flex justify-between items-center">
-        <p class="text-sm"><svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#6ddb23" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"/></svg>Online:</p>
-        <p class="text-sm font-semibold">{{ usersOnline }}</p>
       </div>
     </div>
   </div>
@@ -37,15 +24,13 @@ export default {
     return {
       activeKey: null,
       keyboardLayout: [
-        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'DEL'],
+        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
         ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '*'],
         ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ', 'Ç'],
         ['!', '"', '·', '$', '%', '&', '/', '(', ')', '=', '?', '¿', '¡'],
         ['Shift', 'Z', 'X', 'C', 'Space', 'V', 'B', 'N', 'M'],
         []
-      ],
-      usersOnline: 50,
-      shiftPressed: false
+      ]
     };
   },
   methods: {
@@ -55,33 +40,16 @@ export default {
     handleKeyDown(event) {
       event.preventDefault();
       event.stopPropagation();
-      if (event.key === "Shift") {
-        this.shiftPressed = true;
+      if (event.key === " ") {
+        this.activeKey = "Space";
+        this.handleKeyClick("Space");
       } else {
-        if (this.shiftPressed) {
-          this.activeKey = event.key.toUpperCase();
-        } else {
-          this.activeKey = event.key.toLowerCase();
-        }
-        this.handleKeyClick(this.activeKey); // Llamamos a handleKeyClick una vez aquí
-      }
-      
-      // Manejo específico para la tecla "DEL"
-      if (event.key === "Delete") {
-        this.activeKey = "DEL";
-        this.handleKeyClick("DEL");
+        this.activeKey = event.key.toUpperCase();
+        this.handleKeyClick(event.key.toUpperCase());
       }
     },
-    handleKeyUp(event) {
-      if (event.key === "Shift") {
-        this.shiftPressed = false;
-      }
-      this.activeKey = null; // Desiluminamos la tecla al soltarla
-      
-      // Desiluminar la tecla "DEL" cuando se suelta
-      if (event.key === "Delete") {
-        this.activeKey = null;
-      }
+    handleKeyUp() {
+      this.activeKey = null;
     }
   },
   mounted() {
@@ -96,70 +64,36 @@ export default {
 </script>
 
 <style scoped>
-/* Estilos específicos del componente, puedes personalizar según tus necesidades */
 @font-face {
   font-family: 'JetBrains Mono';
   src: url('~/assets/fonts/JetBrainsMonoNL-Thin.ttf') format('truetype');
   font-weight: normal;
   font-style: normal;
 }
-
 body {
   overflow-y: hidden;
+  /* Oculta el scroll vertical */
 }
-
-.kw-container {
-  display: grid;
-  grid-template-columns: 5fr 0.5fr auto; /* Una columna para el teclado y una para el sidebar */
-}
-
-.kw {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
 .container {
-  margin-top: 10rem;
+  margin-top: 14rem;
   width: 65%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
 }
-
-.sidebar {
-  margin-top: 10rem;
-}
-
-.content-wrapper {
-  width: 100%;
-}
-
-.text-style, .keyboard {
-  width: 100%;
-}
-
 .text-style {
   color: white;
   font-size: 40px;
   font-family: 'JetBrains Mono', monospace;
-  align-self: flex-start;
 }
-
 .keyboard {
   display: flex;
   flex-direction: column;
-  align-items: flex-start; /* Alinea el teclado a la izquierda */
 }
-
 .row {
   display: flex;
   margin-bottom: 5px;
 }
-
 .key {
-  width: 65px;
-  height: 65px;
+  width: 60px;
+  height: 60px;
   background-color: #ccc;
   border: 1px solid #ccc;
   display: flex;
@@ -169,17 +103,10 @@ body {
   border-radius: 5px;
   cursor: pointer;
 }
-
-.space-key, .del-key {
-  flex-grow: 6;
-  /* Hace que la tecla "Space" y "DEL" ocupe 3 veces el espacio horizontal */
-  font-size: 18px; /* Ajustamos el tamaño del texto */
+.space-key {
+  flex-grow: 3;
+  /* Hace que la tecla "Space" ocupe 3 veces el espacio horizontal */
 }
-
-h2 {
-  border-bottom: 1px solid #ccc;
-}
-
 .active {
   background-color: #ffffff;
   color: rgb(0, 0, 0);
